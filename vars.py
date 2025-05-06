529e39d17ea21bc7fd3e4c023e9ef7cd12051487
luciferontelegram <newlygamer03@gmail.com>
2025-05-06 13:30:59 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,8 @@
+# Don't Remove Credit Tg - @Tushar0125
+# Ask Doubt on telegram @Tushar0125
+
+from os import environ
+
+API_ID = int(environ.get("API_ID", "25718812")) #Replace with your api id
+API_HASH = environ.get("API_HASH", "7ecc7924460c092f77af1307c8709b02") #Replace with your api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", "7794654405:AAFOi590gmOIzY-eRC8oDr9GxcXnUVhtpkU") #Replace with your bot token
